73b09370f0e662b0f7f29ccb727db03cf3f42a4f
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-04 23:46:55 -0800

    remove old comments '

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors")
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,md,mdx}',
@@ -17,6 +16,5 @@ module.exports = {
       },
     },
   },
-//  plugins: [require('@tailwindcss/typography')],
 darkMode: "class",
 };
